3b10e6fecd3e9e0ed7e42aa9aecf04d65a4a3789
Srushti Mule <srushti6206@gmail.com>
2026-08-09 12:41:23 +0530

    ✅ Duplicate Emails | Easy | SQL | Runtime: 399 ms (56.16%) | Memory: 0B | 2026-08-09

--- easy/0182-duplicate-emails/solution.sql
+++ easy/0182-duplicate-emails/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT e.name as Employee from Employee e
-JOIN Employee m 
-on e.managerId = m.id
-WHERE e.salary > m.salary;
+SELECT email as Email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
