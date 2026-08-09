811fe53ec7e5bf84bae27b7a412e47f0bbb2bb75
Srushti Mule <srushti6206@gmail.com>
2026-08-09 12:07:42 +0530

    ✅ Nth Highest Salary | Medium | SQL | Runtime: 908 ms (5.02%) | Memory: 0B | 2026-08-09

--- medium/0177-nth-highest-salary/solution.sql
+++ medium/0177-nth-highest-salary/solution.sql
@@ -1,14 +1,13 @@
-CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-  RETURN (
-      # Write your MySQL query statement below.
-    SELECT MAX(salary)
-        FROM Employee e1
-        WHERE (
-            SELECT COUNT(DISTINCT e2.salary)
-            FROM Employee e2
-            WHERE e2.salary > e1.salary
-        ) = N - 1
-  );
+  RETURN (
+      # Write your MySQL query statement below.
+    SELECT MAX(salary)
+        FROM Employee e1
+        WHERE (
+            SELECT COUNT(DISTINCT e2.salary)
+            FROM Employee e2
+            WHERE e2.salary > e1.salary
+        ) = N - 1
+  );
 END
-
