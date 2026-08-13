d1fbdef70d4658f4fda7998a4af1daf119313501
Srushti Mule <srushti6206@gmail.com>
2026-08-13 20:08:19 +0530

    ✅ Customer Placing the Largest Number of Orders | Easy | SQL | Runtime: 500 ms (51.42%) | Memory: 0B | 2026-08-13

--- easy/0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ easy/0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-SELECT customer_number FROM Orders
-group by customer_number 
-order by count(*) desc
-limit 1 ;
+# Write your MySQL query statement below
+SELECT customer_number 
+FROM Orders
+group by customer_number 
+order by count(*) desc
+limit 1 ;
