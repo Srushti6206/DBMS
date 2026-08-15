524466aa551e54310406954c0e87311125f23199
Srushti Mule <srushti6206@gmail.com>
2026-08-15 17:58:30 +0530

    ✅ Sales Person | Easy | SQL | Runtime: 159 ms | Memory: 0B | 2026-08-15

--- easy/0607-sales-person/solution.sql
+++ easy/0607-sales-person/solution.sql
@@ -2,8 +2,7 @@
 SELECT s.name
 FROM SalesPerson s
 WHERE NOT EXISTS (
-    SELECT 1
-    FROM Orders o
+    SELECT * FROM Orders o
     JOIN Company c
         ON o.com_id = c.com_id
     WHERE o.sales_id = s.sales_id
