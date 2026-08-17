ebc1fc5e498445fd71a015c807a830f7e394a33b
Srushti Mule <srushti6206@gmail.com>
2026-08-17 22:12:02 +0530

    ✅ Actors and Directors Who Cooperated At Least Three Times | Easy | SQL | Runtime: 391 ms (56.48%) | Memory: 0B | 2026-08-17

--- easy/1050-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
+++ easy/1050-actors-and-directors-who-cooperated-at-least-three-times/solution.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-SELECT actor_id , director_id 
-FROM ActorDirector
-GROUP BY actor_id , director_id
-HAVING COUNT(*) >= 3 ;
+# Write your MySQL query statement below
+SELECT actor_id , director_id 
+FROM ActorDirector
+GROUP BY actor_id , director_id
+HAVING COUNT(*) >= 3 ;
