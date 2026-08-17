05ed9ab530cf05d575b905bdd676c3ef6bdb4875
Srushti Mule <srushti6206@gmail.com>
2026-08-17 22:16:44 +0530

    ✅ Classes With at Least 5 Students | Easy | SQL | Runtime: 350 ms (53.69%) | Memory: 0B | 2026-08-17

--- easy/0596-classes-with-at-least-5-students/solution.sql
+++ easy/0596-classes-with-at-least-5-students/solution.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-SELECT class FROM Courses
-GROUP BY class
-HAVING COUNT(class) >= 5;
-
+# Write your MySQL query statement below
+SELECT class FROM Courses
+GROUP BY class
+HAVING COUNT(class) >= 5;
