67bccf6865ee976fe07cf25cb809cac99d308e99
Srushti Mule <srushti6206@gmail.com>
2026-08-18 20:17:41 +0530

    ✅ Article Views I | Easy | SQL | Runtime: 484 ms (32.13%) | Memory: 0B | 2026-08-18

--- easy/1148-article-views-i/solution.sql
+++ easy/1148-article-views-i/solution.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT distinct author_id as id FROM Views
-where author_id = viewer_id
-order by id ;
+# Write your MySQL query statement below
+SELECT distinct author_id as id FROM Views
+where author_id = viewer_id
+order by id ;
