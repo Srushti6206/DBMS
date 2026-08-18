b0adc8b6ae67ca19934f042cff90ba1dc80e81e4
Srushti Mule <srushti6206@gmail.com>
2026-08-18 19:22:14 +0530

    ✅ Project Employees I | Easy | SQL | Runtime: 1007 ms (5.00%) | Memory: 0B | 2026-08-17

--- easy/1075-project-employees-i/solution.sql
+++ easy/1075-project-employees-i/solution.sql
@@ -1,7 +1,7 @@
-# Write your MySQL query statement below
-SELECT p.project_id ,
-        round(avg(e.experience_years),2) AS average_years
-FROM Project p
-JOIN Employee e
-ON e.employee_id = p.employee_id
-GROUP BY p.project_id ;
+# Write your MySQL query statement below
+SELECT p.project_id ,
+        round(avg(e.experience_years),2) AS average_years
+FROM Project p
+JOIN Employee e
+ON e.employee_id = p.employee_id
+GROUP BY p.project_id ;
