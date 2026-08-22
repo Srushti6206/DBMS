f167b36087b925bfdc9c4f8094b8529675169e51
Srushti Mule <srushti6206@gmail.com>
2026-08-22 21:00:54 +0530

    ✅ Percentage of Users Attended a Contest | Easy | SQL | Runtime: 1072 ms (50.54%) | Memory: 0B | 2026-08-22

--- easy/1633-percentage-of-users-attended-a-contest/solution.sql
+++ easy/1633-percentage-of-users-attended-a-contest/solution.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-SELECT p.product_id , ROUND(COALESCE(SUM(p.price*u.units)/SUM(u.units) ,0),2) AS average_price
-FROM Prices p 
-LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id 
-AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id ;
-
-
- 
+SELECT contest_id ,
+    ROUND(COUNT(DISTINCT user_id) * 100 /(SELECT COUNT(*) FROM Users),2) 
+    AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC , contest_id ASC ;
