412b6342ddafd69ea65ff76eedb5e6bb9c158b2d
Srushti Mule <srushti6206@gmail.com>
2026-08-22 20:20:52 +0530

    ✅ Customer Who Visited but Did Not Make Any Transactions | Easy | SQL | Runtime: 114 ms | Memory: 0B | 2026-08-22

--- easy/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
+++ easy/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -1,8 +1,5 @@
 # Write your MySQL query statement below
-SELECT v.customer_id , COUNT(*) as count_no_trans
-FROM Visits v
-LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id 
-WHERE t.transaction_id IS NULL
-GROUP BY v.customer_id ;
-
+SELECT u.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI u
+ON e.id = u.id;
