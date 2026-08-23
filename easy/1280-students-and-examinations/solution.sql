d32884d5eb05d5aebf88277b6fdc63013d661f76
Srushti Mule <srushti6206@gmail.com>
2026-08-23 18:37:14 +0530

    ✅ Students and Examinations | Easy | SQL | Runtime: 1103 ms (53.80%) | Memory: 0B | 2026-08-23

--- easy/1280-students-and-examinations/solution.sql
+++ easy/1280-students-and-examinations/solution.sql
@@ -1,12 +1,11 @@
 # Write your MySQL query statement below
-SELECT a1.machine_id,
-    ROUND(AVG(a2.timestamp - a1.timestamp) , 3) AS processing_time
-FROM Activity a1
-JOIN Activity a2
-    ON a1.machine_id = a2.machine_id
-    AND a1.process_id = a2.process_id
-    AND a1.activity_type = 'start'
-    AND a2.activity_type = 'end'
-GROUP BY a1.machine_id;
+SELECT s.student_id , s.student_name , su.subject_name ,       
+        COUNT(e.subject_name) AS attended_exams
+FROM Students s 
+CROSS JOIN Subjects su
+LEFT JOIN Examinations e 
+    ON s.student_id = e.student_id 
+    AND su.subject_name = e.subject_name
+GROUP BY s.student_id , s.student_name , su.subject_name
+ORDER BY s.student_id , su.subject_name;
 
- 
