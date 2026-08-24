28f35923a6e0d68a22adb152597594f0a8a23c0f
Srushti Mule <srushti6206@gmail.com>
2026-08-24 22:31:47 +0530

    ✅ Managers with at Least 5 Direct Reports | Medium | SQL | Runtime: 332 ms (93.61%) | Memory: 0B | 2026-08-24

--- medium/0570-managers-with-at-least-5-direct-reports/solution.sql
+++ medium/0570-managers-with-at-least-5-direct-reports/solution.sql
@@ -1,9 +1,8 @@
-# Write your MySQL query statement below
-SELECT e1.name from Employee e1
-JOIN Employee e2 
-ON e1.id = e2.managerID
-GROUP BY e1.id , e1.name
-having count(e2.id) >= 5 ;
+# Write your MySQL query statement below
+SELECT e1.name from Employee e1
+JOIN Employee e2 
+ON e1.id = e2.managerID
+GROUP BY e1.id , e1.name
+having count(e2.id) >= 5 ;
 
-
-
+# Direct report : An employee whose managerId equals that manager's id
