afef54448f7b6e7eaaa1d7af7087c8463a7487e5
Srushti Mule <srushti6206@gmail.com>
2026-08-27 22:57:22 +0530

    ✅ Find Total Time Spent by Each Employee | Easy | SQL | Runtime: 565 ms (63.17%) | Memory: 0B | 2026-08-27

--- easy/1741-find-total-time-spent-by-each-employee/solution.sql
+++ easy/1741-find-total-time-spent-by-each-employee/solution.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-SELECT event_day as day , emp_id , SUM(out_time - in_time) as total_time
-FROM Employees 
-GROUP BY emp_id , event_day ;
+# Write your MySQL query statement below
+SELECT event_day as day , emp_id , SUM(out_time - in_time) as total_time
+FROM Employees 
+GROUP BY emp_id , event_day ;
