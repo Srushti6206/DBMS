84d152b17d5558236692de6d1d397830c7fa7e6f
Srushti Mule <srushti6206@gmail.com>
2026-08-28 23:02:04 +0530

    ✅ Employees With Missing Information | Easy | SQL | Runtime: 200 ms | Memory: 0B | 2026-08-28

--- easy/1965-employees-with-missing-information/solution.sql
+++ easy/1965-employees-with-missing-information/solution.sql
@@ -2,4 +2,5 @@
 SELECT e.employee_id FROM Employees e
 JOIN Salaries s 
 ON e.employee_id = s.employee_id 
-WHERE e.name IS NULL OR s.salary IS NULL ;
+WHERE e.name IS NULL OR s.salary IS NULL 
+Order by employee_id ;
