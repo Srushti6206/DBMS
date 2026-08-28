5c3cc395d19e32198733c35e35863f30950c8f4a
Srushti Mule <srushti6206@gmail.com>
2026-08-28 23:12:31 +0530

    ✅ Employees With Missing Information | Easy | SQL | Runtime: 96 ms | Memory: 0B | 2026-08-28

--- easy/1965-employees-with-missing-information/solution.sql
+++ easy/1965-employees-with-missing-information/solution.sql
@@ -3,8 +3,8 @@ LEFT JOIN Salaries s 
 ON e.employee_id = s.employee_id 
 WHERE e.name IS NULL 
 UNION
-SELECT e.employee_id FROM Employees e
-LEFT JOIN Salaries s 
+SELECT s.employee_id FROM Salaries s
+LEFT JOIN Employees e 
 ON e.employee_id = s.employee_id 
 WHERE s.salary IS NULL
 Order by employee_id ;
