7a2b1759e8da7ab526fdbd4823c707f1dd55bea4
Srushti Mule <srushti6206@gmail.com>
2026-08-28 23:09:02 +0530

    ✅ Employees With Missing Information | Easy | SQL | Runtime: 120 ms | Memory: 0B | 2026-08-28

--- easy/1965-employees-with-missing-information/solution.sql
+++ easy/1965-employees-with-missing-information/solution.sql
@@ -1,6 +1,11 @@
-# Write your MySQL query statement below
 SELECT e.employee_id FROM Employees e
 LEFT JOIN Salaries s 
 ON e.employee_id = s.employee_id 
-WHERE e.name IS NULL OR s.salary IS NULL 
+WHERE e.name IS NULL 
+UNION
+SELECT e.employee_id FROM Employees e
+LEFT JOIN Salaries s 
+ON e.employee_id = s.employee_id 
+WHERE s.salary IS NULL
 Order by employee_id ;
+# Write your MySQL query statement below
