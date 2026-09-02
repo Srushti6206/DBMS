28d975a868fc49ab386cbd5f85dc8a147dd3f2f6
Srushti Mule <srushti6206@gmail.com>
2026-09-02 22:04:49 +0530

    ✅ Top Travellers | Easy | SQL | Runtime: 117 ms | Memory: 0B | 2026-09-02

--- easy/1407-top-travellers/solution.sql
+++ easy/1407-top-travellers/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-SELECT u.name , COALESCE(SUM(r.distance),0) as travelled_distance 
+SELECT u.name , SUM(r.distance) as travelled_distance 
 FROM Users u
 LEFT JOIN Rides r
 ON u.id = r.user_id 
