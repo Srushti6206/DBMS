715b2dca3cdd12dc0ad02aca51716583a97c6826
Srushti Mule <srushti6206@gmail.com>
2026-09-02 22:07:26 +0530

    ✅ Top Travellers | Easy | SQL | Runtime: 97 ms | Memory: 0B | 2026-09-02

--- easy/1407-top-travellers/solution.sql
+++ easy/1407-top-travellers/solution.sql
@@ -3,5 +3,5 @@ SELECT u.name , COALESCE(SUM(r.distance),0) as travelled_distance 
 FROM Users u
 LEFT JOIN Rides r
 ON u.id = r.user_id 
-GROUP BY u.name 
+GROUP BY u.id , u.name 
 ORDER BY r.distance DESC , u.name ASC ;
