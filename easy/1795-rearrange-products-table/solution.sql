c8be2508a69cc9cbd3da706cf4484fe8bcf95886
Srushti Mule <srushti6206@gmail.com>
2026-09-02 22:21:37 +0530

    ✅ Rearrange Products Table | Easy | SQL | Runtime: 69 ms | Memory: 0B | 2026-09-02

--- easy/1795-rearrange-products-table/solution.sql
+++ easy/1795-rearrange-products-table/solution.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT product_id , store1 FROM Products
+SELECT product_id , store1 AS price FROM Products
 WHERE store1 IS NOT NULL 
 UNION
-SELECT product_id , store2 FROM Products
+SELECT product_id , store2 AS price FROM Products
 WHERE store2 IS NOT NULL 
 UNION
-SELECT product_id , store3 FROM Products
+SELECT product_id , store3 AS price FROM Products
 WHERE store3 IS NOT NULL ;
