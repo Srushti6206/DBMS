e6a6816b7179b0068cf2f8f9bd09fa587dd6a931
Srushti Mule <srushti6206@gmail.com>
2026-09-02 22:15:26 +0530

    ✅ Top Travellers | Easy | SQL | Runtime: 1019 ms (35.04%) | Memory: 0B | 2026-09-02

--- easy/1407-top-travellers/solution.sql
+++ easy/1407-top-travellers/solution.sql
@@ -1,10 +1,9 @@
-LEFT JOIN Rides r
-ON u.id = r.user_id 
-GROUP BY u.id , u.name 
-ORDER BY travelled_distance DESC , u.name ASC ;
+# Write your MySQL query statement below
+SELECT u.name , COALESCE(SUM(r.distance),0) as travelled_distance 
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id 
+GROUP BY u.id , u.name 
+ORDER BY travelled_distance DESC , u.name ASC ;
 
 
-
-FROM Users u
-# Write your MySQL query statement below
-SELECT u.name , COALESCE(SUM(r.distance),0) as travelled_distance 
