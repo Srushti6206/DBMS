87686e36fe3d481f3ffafc84c662f70c20565012
Srushti Mule <srushti6206@gmail.com>
2026-09-06 23:30:52 +0530

    ✅ The Number of Employees Which Report to Each Employee | Easy | SQL | Runtime: 124 ms | Memory: 0B | 2026-09-06

---
+++ easy/1731-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT m.employee_id , m.name , COUNT(e.employee_id) AS reports_count , 
+    ROUND(AVG(e.age),0) AS average_age
+FROM Employees m
+JOIN  Employees e
+ON m.employee_id = e.employee_id
+GROUP BY m.employee_id , m.name
+ORDER BY m.employee_id;
