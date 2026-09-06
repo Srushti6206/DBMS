33a8c89cc549a7e3b3a31a1a1bd58589cb361482
Srushti Mule <srushti6206@gmail.com>
2026-09-06 23:18:16 +0530

    ✅ Employees Whose Manager Left the Company | Easy | SQL | Runtime: 65 ms | Memory: 0B | 2026-09-06

--- easy/1978-employees-whose-manager-left-the-company/solution.sql
+++ easy/1978-employees-whose-manager-left-the-company/solution.sql
@@ -2,6 +2,6 @@
 SELECT e1.employee_id FROM Employees e1
 JOIN Employees e2
 ON e1.employee_id = e2.manager_id
-where e1.salary <= 30000 AND 
+where e1.salary < 30000 AND 
      e2.manager_id IS NULL ;
 
