026b1f174f07a17b9bd452e802ea86c5e71b6797
Srushti Mule <srushti6206@gmail.com>
2026-09-06 23:20:55 +0530

    ✅ Employees Whose Manager Left the Company | Easy | SQL | Runtime: 82 ms | Memory: 0B | 2026-09-06

--- easy/1978-employees-whose-manager-left-the-company/solution.sql
+++ easy/1978-employees-whose-manager-left-the-company/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT e1.employee_id FROM Employees e1
-JOIN Employees e2
+LEFT JOIN Employees e2
 ON e1.employee_id = e2.manager_id
 where e1.salary < 30000 AND 
      e2.manager_id IS NULL ;
