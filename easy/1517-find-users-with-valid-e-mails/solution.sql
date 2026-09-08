6f0cdaa9f8750ef2823ffffa8d7039e74e892994
Srushti Mule <srushti6206@gmail.com>
2026-09-08 18:24:41 +0530

    ✅ Find Users With Valid E-Mails | Easy | SQL | Runtime: 89 ms | Memory: 0B | 2026-09-08

--- easy/1517-find-users-with-valid-e-mails/solution.sql
+++ easy/1517-find-users-with-valid-e-mails/solution.sql
@@ -1,5 +1,6 @@
-# Write your MySQL query statement below
-SELECT * FROM Users
-WHERE LOWER(mail) REGEXP '^[a-zA-Z][-A-Za-z0-9_.]*@leetcode\\.com$'
-ORDER BY user_id ;
+# Write your MySQL query statement below
+SELECT * FROM Users
+WHERE mail REGEXP '^[a-zA-Z][-A-Za-z0-9_.]*@leetcode\\.com$'
+
+ORDER BY user_id ;
 
